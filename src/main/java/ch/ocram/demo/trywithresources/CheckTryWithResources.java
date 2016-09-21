7d542243b6ef65b68cd370243060b4bdf0f23d4b
PoliM <marco.a.poli@gmail.com>
2016-09-21 22:44:19 +0200

    messages unified

--- src/main/java/ch/ocram/demo/trywithresources/CheckTryWithResources.java
+++ src/main/java/ch/ocram/demo/trywithresources/CheckTryWithResources.java
@@ -8,39 +8,39 @@ public class CheckTryWithResources {
   public String doSomething(Config cfg) throws IOException {
     try (Closeable c = getResource(cfg.failOnGetResource, cfg.failOnClose)) {
       callSomeBusinessLogic(cfg.failOnBusinessLogic);
-      return "normal";
+      return "normallyExecuted";
     } catch (Exception ex) {
       handleException(cfg.failOnExceptionHandling);
       return "exceptionHandeled";
     } finally {
       doFinally(cfg.failOnFinally);
       if (cfg.returnInFinally) {
-        return "returnInFinally";
+        return "returnedInFinally";
       }
     }
   }
 
   private void doFinally(boolean failOnFinally) {
     if (failOnFinally) {
-      throw new RuntimeException("failOnFinally");
+      throw new RuntimeException("failedOnFinally");
     }
   }
 
   private void handleException(boolean failOnExceptionHandling) {
     if (failOnExceptionHandling) {
-      throw new RuntimeException("failOnExceptionHandling");
+      throw new RuntimeException("failedOnExceptionHandling");
     }
   }
 
   private void callSomeBusinessLogic(boolean failOnBusinessLogic) {
     if (failOnBusinessLogic) {
-      throw new RuntimeException("failOnBusinessLogic");
+      throw new RuntimeException("failedOnBusinessLogic");
     }
   }
 
   private Closeable getResource(boolean failOnGetResource, boolean failOnClose) {
     if (failOnGetResource) {
-      throw new RuntimeException("failOnGetResource");
+      throw new RuntimeException("failedOnGetResource");
     }
 
     return () -> {
